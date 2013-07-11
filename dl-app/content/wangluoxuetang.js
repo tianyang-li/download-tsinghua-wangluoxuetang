bb04c4138d93e47919af9519004d85b4d647dcbd
Tianyang Li <ty@li-tianyang.com>
2013-07-11 23:43:38 +0800

    why is the window i opened null?

--- dl-app/content/wangluoxuetang.js
+++ dl-app/content/wangluoxuetang.js
@@ -8,27 +8,40 @@ WLXT.DowloadData = {
      * refers to browser window opening WLXT
      */
     learnWindow : null,
+
 };
 
-/*
- * open learn.tsinghua.edu.cn in a new window
- */
-WLXT.DowloadData.openLearn = function() {
-    var strWindowFeatures = ""
-    this.learnWindow = window.open("http://learn.tsinghua.edu.cn", "wlxt_dl_window", strWindowFeatures);
+WLXT.DowloadData.onPageLoad = function(aEvent) {
+    WLXT.DowloadData.handlePageLoad();
+};
+
+WLXT.DowloadData.handlePageLoad = function() {
+    alert(WLXT.DowloadData.learnWindow);
+};
+
+WLXT.DowloadData.init = function() {
+    if (gBrowser) {
+        gBrowser.addEventListener("DOMContentLoaded", WLXT.DowloadData.onPageLoad, false);
+    }
 };
 
 /*
  * change DOM of WLXT login page
  */
 WLXT.DowloadData.modifyLearnLogin = function() {
-    if (this.learnWindow == null) {
-        alert(1);
+    if (WLXT.DowloadData.learnWindow) {
+        var loginTableBody = WLXT.DowloadData.learnWindow.document.getElementsByTagName('body')[0].getElementsByTagName('table')[4].getElementsByTagName('tbody')[0];
+        var notifyUserCell = loginTableBody.insertRow(0).insertCell(0);
+        notifyUserCell.innerHTML = '<b style="color: red">下载网络学堂从这里登录</b>';
     }
-    var loginTableBody = this.learnWindow.document.getElementsByTagName('body')[0].getElementsByTagName('table')[4].getElementsByTagName('tbody')[0];
-    var notifyUserCell = loginTableBody.insertRow(0).insertCell(0);
-    notifyUserCell.innerHTML = '<b style="color: red">下载网络学堂从这里登录</b>';
+};
 
+/*
+ * open learn.tsinghua.edu.cn in a new window
+ */
+WLXT.DowloadData.openLearn = function() {
+    var strWindowFeatures = "";
+    WLXT.DowloadData.learnWindow = window.open("http://learn.tsinghua.edu.cn", "wlxt_dl_window", strWindowFeatures);
 };
 
 WLXT.BrowserOverlay = {
@@ -43,6 +56,11 @@ WLXT.BrowserOverlay = {
 
     startDownload : function(aEvent) {
         WLXT.DowloadData.openLearn();
-        WLXT.DowloadData.modifyLearnLogin();
     }
 };
+
+window.addEventListener("load", function load(event) {
+    window.removeEventListener("load", load, false);
+    //remove listener, no longer needed
+    WLXT.DowloadData.init();
+}, false);
