f3d8e09d77995a89828ce7a7e0b245ae92c3c953
Tianyang Li <ty@li-tianyang.com>
2013-07-12 15:53:56 +0800

    can modify login page DOM now

--- dl-app/content/wangluoxuetang.js
+++ dl-app/content/wangluoxuetang.js
@@ -2,21 +2,17 @@ if ("undefined" == typeof (WLXT)) {
     var WLXT = {};
 };
 
-WLXT.DowloadData = {
-
-    /*
-     * refers to browser window opening WLXT
-     */
-    learnWindow : null,
-
-};
+WLXT.DowloadData = {};
 
 WLXT.DowloadData.onPageLoad = function(aEvent) {
-    WLXT.DowloadData.handlePageLoad();
-};
-
-WLXT.DowloadData.handlePageLoad = function() {
-    alert(WLXT.DowloadData.learnWindow);
+    if (aEvent.target.URL == "http://learn.tsinghua.edu.cn/") {
+        /*
+         * change DOM of WLXT login page
+         */
+        var loginTableBody = aEvent.target.getElementsByTagName('body')[0].getElementsByTagName('table')[4].getElementsByTagName('tbody')[0];
+        var notifyUserCell = loginTableBody.insertRow(0).insertCell(0);
+        notifyUserCell.innerHTML = '<b style="color: red">下载网络学堂从这里登录</b>';
+    }
 };
 
 WLXT.DowloadData.init = function() {
@@ -26,22 +22,11 @@ WLXT.DowloadData.init = function() {
 };
 
 /*
- * change DOM of WLXT login page
- */
-WLXT.DowloadData.modifyLearnLogin = function() {
-    if (WLXT.DowloadData.learnWindow) {
-        var loginTableBody = WLXT.DowloadData.learnWindow.document.getElementsByTagName('body')[0].getElementsByTagName('table')[4].getElementsByTagName('tbody')[0];
-        var notifyUserCell = loginTableBody.insertRow(0).insertCell(0);
-        notifyUserCell.innerHTML = '<b style="color: red">下载网络学堂从这里登录</b>';
-    }
-};
-
-/*
  * open learn.tsinghua.edu.cn in a new window
  */
 WLXT.DowloadData.openLearn = function() {
     var strWindowFeatures = "";
-    WLXT.DowloadData.learnWindow = window.open("http://learn.tsinghua.edu.cn", "wlxt_dl_window", strWindowFeatures);
+    window.open("http://learn.tsinghua.edu.cn", "wlxt_dl_window", strWindowFeatures);
 };
 
 WLXT.BrowserOverlay = {
@@ -64,3 +49,4 @@ window.addEventListener("load", function load(event) {
     //remove listener, no longer needed
     WLXT.DowloadData.init();
 }, false);
+
